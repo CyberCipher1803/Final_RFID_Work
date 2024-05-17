55ff4078914318e22232468757146938f113dad9
CyberCipher1803 <156462705+CyberCipher1803@users.noreply.github.com>
2024-05-17 10:32:31 -0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,22 @@
+def ServoTurnLong():
+    pins.servo_write_pin(AnalogPin.P16, 180)
+    basic.pause(2000)
+    pins.servo_write_pin(AnalogPin.P16, 0)
+def allOff():
+    pins.servo_write_pin(AnalogPin.P16, 0)
+    pins.set_audio_pin_enabled(False)
+def Buzzer():
+    pins.set_audio_pin(AnalogPin.P8)
+def ServoTurnShort():
+    pins.servo_write_pin(AnalogPin.P16, 180)
+    basic.pause(500)
+    pins.servo_write_pin(AnalogPin.P16, 0)
+    basic.pause(500)
+serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BAUD_RATE9600)
+fob = "0796AEA56D57"
+ServoTurnShort()
+
+def on_forever():
+    if serial.read_string() == fob:
+        ServoTurnLong()
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,30 @@
+function ServoTurnLong () {
+    pins.servoWritePin(AnalogPin.P16, 180)
+    basic.pause(2000)
+    pins.servoWritePin(AnalogPin.P16, 0)
+}
+function allOff () {
+    pins.servoWritePin(AnalogPin.P16, 0)
+    pins.setAudioPinEnabled(false)
+}
+function Buzzer () {
+    pins.setAudioPin(AnalogPin.P8)
+}
+function ServoTurnShort () {
+    pins.servoWritePin(AnalogPin.P16, 180)
+    basic.pause(500)
+    pins.servoWritePin(AnalogPin.P16, 0)
+    basic.pause(500)
+}
+serial.redirect(
+SerialPin.P0,
+SerialPin.P1,
+BaudRate.BaudRate9600
+)
+let fob = "0796AEA56D57"
+ServoTurnShort()
+basic.forever(function () {
+    if (serial.readString() == fob) {
+        ServoTurnLong()
+    }
+})
